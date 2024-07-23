e7c0c43100f74eb92ee763c2ccd60d5a48aae6ff
许大仙 <1900919313@qq.com>
2024-07-23 16:24:10 +0800

    c

--- docs/.vitepress/sidebar/index.ts
+++ docs/.vitepress/sidebar/index.ts
@@ -13,14 +13,7 @@ export const sidebar: DefaultTheme.Sidebar = {
     {
       text: '前端学科',
       link: `/notes/03_web-design/`
-    },
-    {
-      text: '其它',
-      collapsed: true,
-      items: [
-        { text: '初识 C 语言', link: `/notes/01_c/01_${commonDirectoryName}/` },
-      ]
-    },
+    }
   ],
 }
 
